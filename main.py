f86c171330e15034a8e73475b0ff67d4b3526707
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2024-01-03 15:00:46 +0530

    ds

--- main.py
+++ main.py
@@ -5,27 +5,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    options = {
-        'margin-top': '0mm',
-        'margin-left': '0mm',
-        'margin-right': '0mm',
-        'margin-bottom': '0mm',
+    options =  {
+        'page-size': 'A4',
+        'margin-top': '0.2in',
+        'margin-right': '0.2in',
+        'margin-bottom': '0.2in',
+        'margin-left': '0.4in',
         'encoding': "UTF-8",
-        'page-size':'A4', 
-        'disable-smart-shrinking': True,
-        'custom-header': [
-            ('Accept-Encoding', 'gzip')
-        ]
-    }
-    dynamic_html = render_template('page1.html')
+        'custom-header': [('Accept-Encoding', 'gzip')]}
+    dynamic_html = render_template('page2.html')
+    print(dynamic_html)
     pdf = pdfkit.from_string(dynamic_html, False, options=options)
 
     response = make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'attachment;filename=test.pdf'
     return response
-
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
